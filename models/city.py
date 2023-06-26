3b7fe99eb267c2f09582ee997ea164f08d6a5af5
NewtonMutugi <newtonmut197@gmail.com>
2023-06-26 18:57:40 +0300

    DBStorage - States and Cities mod

--- models/city.py
+++ models/city.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
+"""This module defines the City class"""
+
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer
-from sqlalchemy import ForeignKey
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from models.place import Place
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """Representation of a city"""
     __tablename__ = 'cities'
-    name = Column(String(128),
-                  nullable=False)
-    state_id = Column(String(60),
-                      ForeignKey('states.id'),
-                      nullable=False)
+
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+
+    if models.storage_type == 'db':
+        places = relationship("Place", backref="cities", cascade="all, delete")
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new City instance"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_type != 'db':
+        @property
+        def places(self):
+            """Returns the list of Place instances with city_id equals to the
+            current City.id"""
+            return [place for place in models.storage.all("Place").values()
+                    if place.city_id == self.id]
