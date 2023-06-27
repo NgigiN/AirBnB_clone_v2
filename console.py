dcf2e71b17c76fac6370a8582944c4b61471b83f
NewtonMutugi <newtonmut197@gmail.com>
2023-06-27 11:55:18 +0300

    do_create

--- console.py
+++ console.py
@@ -118,46 +118,45 @@ class HBNBCommand(cmd.Cmd):
         if not args:
             print("** class name missing **")
             return
-    
+
         arg_list = args.split()
         class_name = arg_list[0]
         params = {}
-    
+
         if class_name not in HBNBCommand.classes:
             print("** class doesn't exist **")
             return
-    
+
         for param in arg_list[1:]:
             # Split the parameter into key and value
             split_param = param.split('=')
             if len(split_param) == 2:
                 key, value = split_param
-    
+
                 # Handle string values
                 if value.startswith('"') and value.endswith('"'):
                     value = value[1:-1].replace('_', ' ').replace('\\"', '"')
-    
+
                 # Handle float values
                 elif '.' in value:
                     try:
                         value = float(value)
                     except ValueError:
                         continue
-                    
+
                 # Handle integer values
                 else:
                     try:
                         value = int(value)
                     except ValueError:
                         continue
-                    
+
                 params[key] = value
-    
+
         new_instance = HBNBCommand.classes[class_name](**params)
         storage.save()
         print(new_instance.id)
         storage.save()
-
 
     def help_create(self):
         """ Help information for the create method """
