b504a078ad3f844d2f7e9cfe6008a5ec62ececc2
NgigiN <ngigi.nyongo@gmail.com>
2023-07-09 18:57:30 +0300

    wow

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,39 +1,10 @@
 #!/usr/bin/env bash
-# Bash script setting up the web server for deployment on web static
-
-apt-get update
-apt-get install -y nginx
-
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared
-
-echo "Hello Ngigi" > /data/web_static/releases/test/index.html
-
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-sudo chown -R ubuntu:ubuntu /data
-
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
-
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-
-    location /redirect_me {
-        return 301 https://www.intouch.org/;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-enabled/default
-
-sudo service nginx restart
+# Script that sets up your web servers for the deployment of web_static
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
