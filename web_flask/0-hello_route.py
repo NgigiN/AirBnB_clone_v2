ebc4e37d9bb9eba8a0091073fec91fd7d9323907
NgigiN <ngigi.nyongo@gmail.com>
2023-07-23 20:59:42 +0300

    fixing error with 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,8 +2,9 @@
 """ Script that starts a Flask web application """
 from flask import Flask
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
-app.route('/', strict_slashes=False)
+app.route('/')
 
 
 def hello():
@@ -18,4 +19,4 @@ def hbnb():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run(host='.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
