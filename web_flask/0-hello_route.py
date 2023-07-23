a5a54b9cb258d3161789d4c888f7fcfbc9d18094
NgigiN <ngigi.nyongo@gmail.com>
2023-07-23 21:23:08 +0300

    fixing error with 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,11 +11,5 @@ def hello():
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
-def hbnb():
-    """ Function that returns HBNB """
-    return 'HBNB'
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
