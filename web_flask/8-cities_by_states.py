4fda6f16d34a09579ec77dc2da0df6b93b47e37e
NgigiN <ngigi.nyongo@gmail.com>
2023-07-24 13:01:14 +0300

    7

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """ Importing flask to run the web app """
-
 from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
 
 app = Flask(__name__)
-
-
-@app.route('/states/list', strict_slashes=False)
-def display_states():
-    """ Renders States list html page displays the states """
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown(self):
     """ Closes the current session """
     storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """ Renders a html page with states and cities"""
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 if __name__ == '__main__':
